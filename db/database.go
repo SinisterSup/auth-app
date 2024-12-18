5b91af5eaa31edaa5bf38f0d774d8f333af279f4
SinisterSup <mheist456@gmail.com>
2024-12-18 23:37:18 +0530

    added mongodb connection

---
+++ db/database.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"log"
+	// "fmt"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = client.Database(os.Getenv("MONGODB_DATABASE"))
+	// fmt.Println("MongoDB connection established")
+	log.Println("Successfully connected to MongoDB!")
+}
